d655db1fa636fab2c90addd9697c755ce996dc33
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-02-27 22:16:37 +0300

    feat(add):index.js create getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  const tr = preferences;
+  var count = 0;
+  for (let i = 0; i < tr.length; i++) {
+    if (
+      tr[tr[tr[i] - 1] - 1] - 1 == i &&
+      tr[tr[i] - 1] > tr[tr[tr[i] - 1] - 1] &&
+      tr[i] < tr[tr[i] - 1]
+    ) {
+      count = ++count;
+    }
+  }
+  return count;
 };
